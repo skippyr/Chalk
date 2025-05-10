f5c5c1c14dcccc44ed85972417d4ec4581e91704
Sherman Rofeman <skippyr.developer@icloud.com>
2025-05-10 20:37:48 -0300

    prepare release v1.0.1
    - Rewrite README.
    - Add missing LICENSE.
    - Customize software pages to make it consistent with other projects.

--- README.md
+++ README.md
@@ -1,28 +1,63 @@
-# Chalk
-## About
-A command line tool that displays the terminal 8-bit colors, useful for debugging. It is available for macOS.
+<p align="center">
+  <img alt="" src="assets/ornament.png" width="1020" />
+</p>
+<h1 align="center">≥v≥v&ensp;Chalk&ensp;≥v≥v</h1>
+<p align="center">
+  <img alt="" src="https://img.shields.io/github/license/skippyr/Chalk?style=plastic&label=%E2%89%A5%20license&labelColor=%2324130e&color=%23b8150d" />
+  &nbsp;
+  <img alt="" src="https://img.shields.io/github/v/tag/skippyr/Chalk?style=plastic&label=%E2%89%A5%20tag&labelColor=%2324130e&color=%23b8150d" />
+  &nbsp;
+  <img alt="" src="https://img.shields.io/github/commit-activity/t/skippyr/Chalk?style=plastic&label=%E2%89%A5%20commits&labelColor=%2324130e&color=%23b8150d" />
+  &nbsp;
+  <img alt="" src="https://img.shields.io/github/stars/skippyr/Chalk?style=plastic&label=%E2%89%A5%20stars&labelColor=%2324130e&color=%23b8150d" />
+</p>
 
-![](Assets/Preview.png)
+## ❡ About
+A command line tool that displays the terminal 8-bit ANSI colors, useful for debugging. It is available for macOS.
 
-## Install
-### Prerequisites
-In order to use this software, the following requirements must be fulfilled:
-- Have the latest version of the Swift Toolchain installed.
+<p align="center">
+  <img alt="" src="assets/preview.png" width="1020" />
+</p>
+<p align="center"><strong>Caption:</strong> a preview of Chalk running in <a href="https://github.com/kovidgoyal/kitty">Kitty</a> on macOS. Shell theme is <a href="https://github.com/skippyr/river_dreams">River Dreams</a> and font is <a href="https://www.monolisa.dev">Monolisa</a> (with fallback to <a href="https://github.com/ryanoasis/nerd-fonts">Symbols Nerd Font</a>). Background is AI art and provided as an wallpaper port by the <a href="https://github.com/skippyr/Chalk">Flamerial</a> palette.</p>
+
+## ❡ Install
+### Dependencies
+The following dependencies must be installed before it:
+- **Git**: it will be used to clone this repository.
+- **Swift Toolchain**: it provides all the tools required to build this software.
+
+Use the macOS Command Line Tools bundle to install all the dependencies listed above. They also come with the install of XCode.
 
 ### Procedures
-- Use `swift` to compile the software:
+- Use `git` to clone this repository:
+
+```zsh
+git clone --depth 1 https://github.com/skippyr/Chalk;
+```
+
+- Access the repository directory using `cd`:
+
+```zsh
+cd Chalk;
+```
+
+- Use `swift` to build the software:
 
 ```zsh
 swift build -c release
 ```
 
-- Add `.build/release/chalk` to your system `PATH` environment variable.
+- Add `.build/release/chalk` to a directory in your system `PATH` environment variable.
+- Reopen your shell and it will be available as a command.
 
-## Help
-If you need help related to this project, open a new issue in its [issues pages](https://gitlab.com/skippyr/Chalk/issues) or send an [e-mail](mailto:skippyr.developer@icloud.com) describing what is going on.
+## ❡ Help
+If you need help related to this project, open a new issue in its [issues pages](https://github.com/skippyr/Chalk/issues) or send an [e-mail](mailto:skippyr.developer@icloud.com) describing what is going on.
 
-## Contributing
-This project is open to review and possibly accept contributions in the form of bug reports and suggestions. If you are interested, send your contribution to its [merge requests](https://gitlab.com/skippyr/Chalk/merge_requests) page or via [e-mail](mailto:skippyr.developer@icloud.com).
+## ❡ Contributing
+This project is open to review and possibly accept contributions in the form of bug reports and suggestions. If you are interested, send your contribution to its [pull requests page](https://github.com/skippyr/Chalk/pulls) or via [e-mail](mailto:skippyr.developer@icloud.com).
 
-## Copyright
-This is free software licensed under the BSD 3-Clause License that comes WITH NO WARRANTY. Refer to the `LICENSE` file that comes in its source code for license and copyright details.
+## ❡ License
+This is free software licensed under the BSD-3-Clause License that comes WITH NO WARRANTY. Refer to the `LICENSE` file that comes in its source code for license and copyright details.
+
+&ensp;
+<p align="center"><sup>– 🐉❤️‍🔥 –</br><strong>≥v≥v&ensp;Here Be Dragons!&ensp;≥v≥</strong><br/>Made with love by 🍒 <a href="https://github.com/skippyr">skippyr</a></sup></p>
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,11 +1,11 @@
 import Rainbow
-import Foundation
+import AppKit
 
 extension String {
     func atCenter(width: Int) -> Self {
         String(repeating: " ", count: (max(0, width - count)) / 2) + self
     }
-    
+
     var isOption: Bool {
         count == 2 && self[startIndex] == "-" && self[index(startIndex, offsetBy: 1)].isLetter || starts(with: "--")
     }
@@ -43,7 +43,7 @@ func printCubeGradient(from initialColor: Int, width: Int) {
     func createTitle(consideringInitialColor initialColor: Int) -> String {
         "Cube Gradient \(initialColor) – \(initialColor + 35)"
     }
-    
+
     let firstTitle = createTitle(consideringInitialColor: initialColor)
     let secondTitle = createTitle(consideringInitialColor: initialColor + 36)
     print("\(firstTitle.bold)\(String(repeating: " ", count: max(0, 51 - firstTitle.count)))\(secondTitle.bold)")
@@ -74,18 +74,73 @@ func printGrayscale() {
 func writeHelp() {
     print(
         """
-        Usage: chalk [OPTIONS]...
-        Displays the terminal 8-bits colors, useful for debugging.
-        
-        AVAILABLE OPTIONS
-            -h, --help     shows the software help instructions.
-            -v, --version  shows the software version.
+        \(":".yellow.bold)\("<>".red.bold)\("::".yellow.bold) \("Usage:".magenta.bold) chalk [\("OPTIONS".cyan.underline)]...
+        Displays the terminal 8-bits ANSI colors, useful for debugging.
+
+        \("❡ AVAILABLE OPTIONS".magenta.bold)
+            \("-h".cyan), \("--help".cyan)        shows the software help instructions.
+            \("-v".cyan), \("--version".cyan)     shows the software version.
+            \("-l".cyan), \("--license".cyan)     shows the software license.
+            \("-g".cyan), \("--repository".cyan)  opens the software repository.
+            \("-m".cyan), \("--email".cyan)       drafts an e-mail to the developer.
         """
     )
 }
 
+func openUrl(_ url: String) {
+    NSWorkspace.shared.open(URL(string: url)!)
+}
+
 func writeVersion() {
-    print("chalk 1.0.0 (https://gitlab.com/skippyr/Chalk)")
+    print("""
+    \("chalk".magenta.bold) v1.0.1 (macOS)
+    Available at: \("https://github.com/skippyr/Chalk".cyan.underline).
+
+    Licensed under the BSD-3-Clause License.
+    Copyright © 2023 Sherman Rofeman <\("skippyr.developer@icloud.com".cyan.underline)>.
+    """)
+}
+
+func writeLicense() {
+    print("""
+    BSD 3-Clause License
+
+    Copyright (c) 2025, Sherman Rofeman <skippyr.developer@icloud.com>
+
+    Redistribution and use in source and binary forms, with or without
+    modification, are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
+
+    2. Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+    3. Neither the name of the copyright holder nor the names of its
+    contributors may be used to endorse or promote products derived from
+    this software without specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+    AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+    IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+    DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+    FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+    DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+    CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+    
+    """)
+}
+
+func openRepository() {
+    openUrl("https://github.com/skippyr/Chalk")
+}
+
+func draftEmailToDeveloper() {
+    openUrl("mailto:skippyr.developer@icloud.com")
 }
 
 if isatty(STDOUT_FILENO) == 0 {
@@ -101,7 +156,6 @@ if terminalWidth < minimumTerminalWidth {
     writeError("the terminal width must be at least \(minimumTerminalWidth) columns.")
     exit(1)
 }
-
 for argument in CommandLine.arguments.dropFirst() {
     switch argument {
     case "-h", "--help":
@@ -110,6 +164,15 @@ for argument in CommandLine.arguments.dropFirst() {
     case "-v", "--version":
         writeVersion()
         exit(0)
+    case "-l", "--license":
+        writeLicense()
+        exit(0)
+    case "-g", "--repository":
+        openRepository()
+        exit(0)
+    case "-m", "--email":
+        draftEmailToDeveloper()
+        exit(0)
     default:
         if argument.isOption {
             writeError("invalid option \"\(argument)\" provided.")
@@ -117,7 +180,6 @@ for argument in CommandLine.arguments.dropFirst() {
         }
     }
 }
-
 printStandardColors()
 print()
 printCubeGradient(from: 16, width: terminalWidth)
